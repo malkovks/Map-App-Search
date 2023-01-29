aaf77b5c0acd01ff9f3ce9e468350fc4fc058be2
Константин Малков <kmalkov_1995@mail.ru>
2023-01-29 10:57:24 +0300

    Initial Commit

---
+++ Map Search App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map Search App
+//
+//  Created by Константин Малков on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
